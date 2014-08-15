4221fdd58b7ca87863c5d29c4881544653d7f5d1
E.Azer Koçulu <azer@kodfabrik.com>
2014-08-15 01:31:34 -0700

    dont set bg if not passed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Brick = require("brick");
 
 module.exports = Brick(function (cover) {
+  if (!cover.url) return;
+
   cover.brick.bind('.cover-background', {
     'style': 'background-image:url(' + cover.url + ')'
   });
